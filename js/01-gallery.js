45aba95155d83882ed18303965e1c54646ae5be7
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-08-07 20:50:23 +0300

    gallery markup added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const markUpGalleryList = createItemMarkUp(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", markUpGalleryList);
+
+function createItemMarkUp(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+    `;
+    })
+    .join("");
+}
+
+console.log(createItemMarkUp(galleryItems));
