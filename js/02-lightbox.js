30311f71648335c330cbbbd57e6402d050a04bc1
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-08-08 16:00:42 +0300

    markup added

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const markUpGalleryList = createItemMarkUp(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+galleryContainer.insertAdjacentHTML("beforeend", markUpGalleryList);
+
+function createItemMarkUp(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
